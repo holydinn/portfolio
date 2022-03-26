9caa374203f9739ac8f057d4eb044f2626c0b0c1
holydinn <diana.imamieva@yandex.ru>
2022-03-26 22:18:04 +0300

    small-fixes

--- js/main.js
+++ js/main.js
@@ -7,13 +7,16 @@ const langChoice = document.querySelector('.lang-choice');
 for(item of navSidebar){
     item.addEventListener('click', ()=>{
         sidebar.classList.remove("active");
+        document.body.classList.remove('hidden');
     })
 }
 
 menuBtn.addEventListener('click', ()=>{
     sidebar.classList.add("active");
+    document.body.classList.add('hidden');
 })
 
 closeBtn.addEventListener('click', ()=>{
     sidebar.classList.remove("active");
+    document.body.classList.remove('hidden');
 })
